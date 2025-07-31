583958b378f7a05d7dd7c36665269e5a8f30315b
SamCodeX01 <txtstring@gmail.com>
2025-07-31 13:11:04 -0300

    Adicionando arquivo no github

---
+++ src/main/java/Main.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
+
+
+/*
+
+Mostrar:
+ *************************************
+ "Dados Iniciais do Cliente"
+    Nome, Tipo da Conta, Saldo Inicial
+ *************************************
+
+Operações:
+
+Digite a opção desejada
+
+1- Consultar saldos
+2- Receber valor
+3- Transferência valor
+4- Sair
+
+Passo a passo:
+
+Inicializar dados do cliente
+Menu de opções
+Visualização de saldo
+Envia valor
+Recebe valor
+
+
+Depois de acabar esse desafio, fazer o capitulo 4 de java da Alura
+
+*/
