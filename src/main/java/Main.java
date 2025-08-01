da2a9a0846f07e551c2204d3d91186cd64c209e5
SamCodeX01 <txtstring@gmail.com>
2025-08-01 14:13:00 -0300

    Desafio finalizado

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -5,7 +5,7 @@ public class Main {
         Scanner sc = new Scanner(System.in);
         String nome, tipoConta;
         int opcao;
-        double saldoInicial, valorRecebido = 0, transferencia = 0, saldoTotalAtualizado;
+        double saldo = 0, valorRecebido = 0, transferencia = 0;
 
         System.out.println("\n**********!!!BEM VINDO AO SEU BANCO!!!**********");
         System.out.print("Nome: ");
@@ -13,7 +13,7 @@ public class Main {
         System.out.print("Tipo da Conta: ");
         tipoConta = sc.nextLine();
         System.out.print("Saldo Inicial: ");
-        saldoInicial = sc.nextDouble();
+        saldo = sc.nextDouble();
 
         while (true) {
 
@@ -23,80 +23,65 @@ public class Main {
                               1- Consultar saldos
                               2- Receber valor
                               3- Transferência valor
-                              4- Sair          
+                              4- Mostrar dados do cliente
+                              5- Sair          
                 """);
 
         System.out.print("Digite a opção desejada: ");
         opcao = sc.nextInt();
-        System.out.println("*****Opção " + opcao + " selecionada!***** ");
+        System.out.println("\n*****Opção " + opcao + " selecionada!***** ");
+        if(opcao == 1){System.out.println("*****Consultar saldos!*****");}
+        else if(opcao == 2){System.out.println("*****Receber valor!*****");}
+        else if(opcao == 3){System.out.println("*****Transferência valor!*****");}
+        else if(opcao == 4){System.out.println("*****Mostrar dados do cliente!*****");}
+        else if(opcao == 5){System.out.println("*****Sair!*****");}
         System.out.print("**************************************************\n");
 
         switch (opcao) {
             case 1:
                 //1- Consultar saldos
-                consultarSaldo(saldoInicial, valorRecebido);
-                System.out.println("Saldo total é: " + consultarSaldo(saldoInicial, valorRecebido));
+                System.out.println("O saldo é: " + saldo);
                 break;
             case 2:
                 //2- Receber valor
                 System.out.print("Qual o valor a ser recebido? ");
                 valorRecebido = sc.nextDouble();
-                saldoTotalAtualizado = saldoInicial + valorRecebido;
+                saldo += valorRecebido;
                 System.out.println("O valor a ser recebido é: " + valorRecebido);
-                System.out.println("Saldo total: " + saldoTotalAtualizado);
+                System.out.println("Saldo total: " + saldo);
                 break;
             case 3:
                 //3-Transferência valor
-                System.out.println("Qual o valor da transferência ser recebido? ");
+                System.out.print("Qual o valor da transferência? ");
                 transferencia = sc.nextDouble();
-                // double saldoInicial, valorRecebido = 0, transferencia = 0;
+
+                if(transferencia > saldo){
+                    System.out.println("Saldo insuficiente para transferência!");
+                }
+                else{
+                    saldo -= transferencia;
+                    System.out.println("Trasnferência realizada: " + transferencia);
+                    System.out.println("Saldo atual: " + saldo);
+                }
                 break;
+
             case 4:
-                System.out.println("Saindo da Aplicação! ");
+                System.out.println("NOME: " + nome);
+                System.out.println("TIPO DE CONTA: " + tipoConta);
+                System.out.println("SALDO ATUAL: " + saldo);
                 break;
+            case 5:
+                System.out.println("Fim da Aplicação! ");
+                System.exit(0);
+                break;
+
+            default:
+                System.out.println("Opção inválida!");
         }
     }
 
     }
-
-    //saldoInicial + valorRecebido - transferencia
-        public static double consultarSaldo(double saldoInicial, double valorRecebido){
-            return  (saldoInicial + valorRecebido);
-        }
-
-//        public static double valorRecebido(double valorRecebido, double saldoInicial){
-//            return (saldoInicial += valorRecebido);
-//        }
-
-      /*  public static double valorTRansferencia(double saldoInicial, double valorRecebido, double transferencia){
-            return ((saldoInicial + valorRecebido) - transferencia);
-
-        // double saldoInicial, valorRecebido = 0, transferencia = 0;
-         */
-
-
 
 }
 
 
-/*
-
-Mostrar:
- *************************************
- "Dados Iniciais do Cliente"
-    Nome, Tipo da Conta, Saldo Inicial
- *************************************
-
-
-Passo a passo:
-
-Inicializar dados do cliente
-Menu de opções
-Visualização de saldo
-Envia valor
-Recebe valor
-
-
-Depois de acabar esse desafio, fazer o capitulo 4 de java da Alura
-
-*/
